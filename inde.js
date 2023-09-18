b83e1bdcb0389e620f68679f209b58faa790cc5b
BelalShah2000 <belalshah960@gmail.com>
2023-09-18 10:40:12 +0530

    first commit

---
+++ inde.js
@@ -0,0 +1,22 @@
+
+
+
+function validatePhoneNumber(phoneNumber) {
+
+    const delExtraNum = phoneNumber.replace(/\D/g, '');
+  
+  
+    if ( delExtraNum.length !== 11 & delExtraNum.length !== 10  ) {
+      return false;
+    }
+  
+    if (delExtraNum.charAt(0) === '0')  {
+      return false;
+    }
+  
+    return true;
+  }
+  
+  
+  console.log(validatePhoneNumber("1(555)555-5555"));
+  
